bf812f92c2c9b8c9405c52fcdaf0392e7fabe884
vinic <vinicius.ares12@gmail.com>
2025-05-06 01:59:36 -0300

    Add Question3: product difference calculation

---
+++ src/ExercisesPart1/Question3.java
@@ -0,0 +1,26 @@
+package ExercisesPart1;
+
+import java.util.Scanner;
+
+public class Question3 {
+    public static void main(String[] args) {
+        // Write a program to read four integer values A, B, C and D.
+        // Then, calculate and display the difference between the
+        // product of A and B and the product of C and D, according to the formula:
+        // DIFFERENCE = (A * B - C * D)
+
+        Scanner keyboard = new Scanner(System.in);
+        int A, B, C, D, Difference;
+
+        A = keyboard.nextInt();
+        B = keyboard.nextInt();
+        C = keyboard.nextInt();
+        D = keyboard.nextInt();
+
+        Difference = (A * B - C * D);
+
+        System.out.println("Difference = " + Difference);
+
+        keyboard.close();
+    }
+}
