d4b469cef78c0591147f1f7596e6fbe0065a7ca7
vinic <vinicius.ares12@gmail.com>
2025-05-06 01:59:36 -0300

    Add Question4: employee salary calculation

---
+++ src/ExercisesPart1/Question4.java
@@ -0,0 +1,28 @@
+package ExercisesPart1;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Question4 {
+    public static void main(String[] args) {
+        // Write a program that reads an employee's number,
+        // the number of hours worked, and the amount he receives per hour.
+        // Then, calculate the employee’s salary. Finally,
+        // display the employee's number and salary, with two decimal places.
+
+        Locale.setDefault(Locale.US);
+        Scanner keyboard = new Scanner(System.in);
+        int EmployeeNum, WorkedHours;
+        double AmmountHour, Salary;
+
+        EmployeeNum = keyboard.nextInt();
+        WorkedHours = keyboard.nextInt();
+        AmmountHour = keyboard.nextDouble();
+
+        Salary = WorkedHours * AmmountHour;
+
+        System.out.printf("NUMBER = %d\nSALARY = U$ %.2f", EmployeeNum, Salary);
+
+        keyboard.close();
+    }
+}
