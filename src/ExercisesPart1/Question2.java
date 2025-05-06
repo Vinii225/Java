616a6c6c2528b2555ac11f4d4427a6c2406e510f
vinic <vinicius.ares12@gmail.com>
2025-05-06 01:59:36 -0300

    Add Question2: circle area calculation

---
+++ src/ExercisesPart1/Question2.java
@@ -0,0 +1,27 @@
+package ExercisesPart1;
+
+import java.util.Locale;
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Question2 {
+    public static void main(String[] args) {
+        // Write a program to read the value of the radius of a circle,
+        // and then display the area of the circle with four decimal
+        // places, as shown in the examples.
+        // Formula for the area: area = π * radius²
+        // Consider the value of π = 3.14159
+
+        Locale.setDefault(Locale.US);
+        Scanner keyboard = new Scanner(System.in);
+        double pi = 3.14159, r, AreaOfCircle;
+
+        r = keyboard.nextDouble();
+
+        AreaOfCircle = pi * Math.pow(r, 2.0);
+
+        System.out.printf("A=%.4f", AreaOfCircle);
+
+        keyboard.close();
+    }
+}
