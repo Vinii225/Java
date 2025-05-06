aadce7071a6a9ced0c268c8aa8346e08f816834f
vinic <vinicius.ares12@gmail.com>
2025-05-06 01:59:35 -0300

    Add Question1: sum of two integers

---
+++ src/ExercisesPart1/Question1.java
@@ -0,0 +1,24 @@
+package ExercisesPart1;
+
+import java.util.Scanner;
+
+public class Question1 {
+    public static void main(String[] args) {
+        // Write a program to read two integer values,
+        // and then display on the screen the sum of
+        // these numbers with an explanatory message,
+        // as shown in the examples.
+
+        Scanner keyboard = new Scanner(System.in);
+        int n1, n2, sum;
+
+        n1 = keyboard.nextInt();
+
+        n2 = keyboard.nextInt();
+
+        sum = n1 + n2;
+
+        System.out.printf("SUM = %d", sum);
+        keyboard.close();
+    }
+}
