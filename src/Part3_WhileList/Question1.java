9af1c21216136fc69504aa0c337ad0c8dd46133a
vinic <vinicius.ares12@gmail.com>
2025-05-13 00:46:37 -0300

    Verify Correct Password

--- src/Part3_WhileList/Question1.java
+++ src/Part3_WhileList/Question1.java
@@ -5,17 +5,17 @@ import java.util.Scanner;
 public class Question1 {
     public static void main(String[] args) {
         // Write a program that repeatedly reads a password until it is valid.
-        // For each incorrect password entered, display the message 
-        // "Invalid Password". When the correct password is entered, 
+        // For each incorrect password entered, display the message
+        // "Invalid Password". When the correct password is entered,
         // display "Access Granted" and end the program. Assume that
         // the correct password is the value 2002.
 
         Scanner keyboard = new Scanner(System.in);
-        int correct_password=2002, verify;
+        int correct_password = 2002, verify;
 
         while (true) {
-            verify=keyboard.nextInt();
-            if (verify==correct_password) {
+            verify = keyboard.nextInt();
+            if (verify == correct_password) {
                 System.out.println("Acess Granted");
                 break;
             } else {
