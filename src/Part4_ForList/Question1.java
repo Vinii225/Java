dd786714f00630886f53df6e3c1dab2539fabcd1
vinic <vinicius.ares12@gmail.com>
2025-05-13 23:00:18 -0300

    New Questions

--- src/Part4_ForList/Question1.java
+++ src/Part4_ForList/Question1.java
@@ -4,18 +4,18 @@ import java.util.Scanner;
 
 public class Question1 {
     public static void main(String[] args) {
-        // Read an integer value X (1 ≤ X ≤ 1000). 
+        // Read an integer value X (1 ≤ X ≤ 1000).
         // Then, display all odd numbers from 1 to X,
         // one value per line, including X if it is odd.
 
         Scanner keyboard = new Scanner(System.in);
         int X;
 
-        X=keyboard.nextInt();
+        X = keyboard.nextInt();
 
-        if (X>=1 && X<=1000) {
-            for (int i=1; i<=X; i++) {
-                if (i % 2!=0) {
+        if (X >= 1 && X <= 1000) {
+            for (int i = 1; i <= X; i++) {
+                if (i % 2 != 0) {
                     System.out.println(i);
                 }
             }
---
+++ src/Part4_ForList/Question2.java
@@ -0,0 +1,13 @@
+package Part4_ForList;
+
+public class Question2 {
+    public static void main(String[] args) {
+        // Read an integer value N. This value will
+        // represent the number of integer values X 
+        // to be read next. For each of these values,
+        // determine whether it is within the interval
+        // [10, 20] or outside it. Display how many of
+        // these values are "in" the interval and how many
+        // are "out", following the format shown in the example.
+    }
+}
